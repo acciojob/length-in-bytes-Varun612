16c1ea8611eb1641385ed7466c6772226d138056
Varun Kandhari <75046190+Varun612@users.noreply.github.com>
2025-04-20 15:40:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 const byteSize = (str) => {
   // write your code here
+	// console.log(byteSize(byteSize)) 
+	let size = new Blob([str]).size;
+	  return size;
 };
 
 // Do not change the code below
